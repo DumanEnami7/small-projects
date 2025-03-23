6dfa6ef62d0bb4c3ce5762e4516050d8c5cd488a
dumanEnami7 <enamiduman189@gmail.com>
2025-03-23 21:41:09 +0430

    upadting the side bar

--- sideBar/script.js
+++ sideBar/script.js
@@ -1,3 +1,7 @@
 $("button").click(function () {
   $(".sideBar").toggleClass("showBar");
 });
+$(".sideBar div").click(function (evt) {
+  evt.preventDefault();
+  $(this).children("ul").toggleClass("showItm");
+});
