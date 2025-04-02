1461584bdbfb5c19cf758beecf2d79e999e06cb3
dumanEnami7 <enamiduman189@gmail.com>
2025-04-02 15:37:04 +0430

    added a small tabbed page

---
+++ tabs/script.js
@@ -0,0 +1,22 @@
+let buttons = document.querySelectorAll("li");
+let parags = document.querySelector("p");
+let msgCount = 0;
+const msgs = [
+  "racoons ARE AWESOME",
+  "black fazzies ARE AWESOME",
+  "crows ARE AWESOME",
+];
+function rstBtns() {
+  buttons.forEach((button) => {
+    button.classList.remove("selected");
+  });
+}
+buttons.forEach((prps, index) => {
+  prps.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    rstBtns();
+    this.classList.add("selected");
+    parags.innerHTML = msgs[index];
+    msgCount++;
+  });
+});
